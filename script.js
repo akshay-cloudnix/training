cd964d003baba776c7b4434d34a713e969b75280
akshay-cloudnix <akshay@cloudnix.com>
2019-08-22 09:51:34 +0530

    Images added

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip({
-        trigger : 'hover',
-    }) 
-    if(isTouchDevice()===false) {
-        $("[rel='tooltip']").tooltip();
-    } 
-});
